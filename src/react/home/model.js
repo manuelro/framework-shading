0434947023b76e1f58970fee43a620e4a1abdac6
Manuel Rocha <manuelro@manu-macbook.local>
2019-07-15 22:40:04 -0600

    couple model with controller and view

--- src/react/home/model.js
+++ src/react/home/model.js
@@ -1,3 +1,3 @@
 export default {
-  greeting: 'Hello World from React!'
+  greeting: 'Hello World from React!!!!'
 }
--- src/vue/footer/model.js
+++ src/vue/footer/model.js
@@ -1,3 +1,3 @@
 export default {
-  greeting: 'Hello World from Vue!'
+  greeting: 'Hello World from Vue!!!!'
 }
